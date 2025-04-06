a1067f4b778151f19da0d1b943f53a541d2a159f
hihassan1998 <hassanihussain1998@gmail.com>
2025-04-06 21:12:59 +0200

    almost completed need redovisngs text

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+// use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ApiController extends AbstractController
+{
+    #[Route("/api", name: "api")]
+    public function api(): Response
+    {
+        $routes = [
+            'api_lucky_number' => 'Route: /api/lucky/number Få json-data för Lucky number',
+            'api_quote' => 'Route: /api/quote Få json-data för dagens offert, datum, tidsstämpel',
+        ];
+
+        return $this->render('api.html.twig', [
+            'routes' => $routes
+        ]);
+    }
+
+}
---
+++ src/Controller/LuckyControllerJson.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerJson
+{
+    #[Route("/api/lucky/number", "api_lucky_number")]
+    public function jsonNumber(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'lucky-number' => $number,
+            'lucky-message' => 'Hi there!',
+        ];
+
+        // return new JsonResponse($data);
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+    #[Route("/api/quote", name: "api_quote")]
+    public function quote(): Response
+    {
+        $quotes = [
+            "Today is your lucky day!",
+            "Greate friends are hard to find",
+            "The fortune cookie never lies!",
+            "A surprise is waiting for you soon.",
+            "Being stress-free is a construct of mind",
+        ];
+
+        $randomQuote = $quotes[array_rand($quotes)];
+
+        $date = new \DateTime();
+
+        $data = [
+            'quote' => $randomQuote,
+            'date' => $date->format('Y-m-d'),
+            'timestamp' => $date->getTimestamp(),
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+}
