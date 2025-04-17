cd8d0fcdd48614eb8418edc31709def0b726a5e0
hihassan1998 <hassanihussain1998@gmail.com>
2025-04-17 18:48:22 +0200

    Add style to buttons, update route logic to play game and dispaly die

--- src/Controller/DiceGameController.php
+++ src/Controller/DiceGameController.php
@@ -27,11 +27,17 @@ class DiceGameController extends AbstractController
         return $this->render('pig/init.html.twig');
     }
     #[Route("/game/pig/init", name: "pig_init_post", methods: ['POST'])]
-    public function initCallback(
-        Request $request,
-        SessionInterface $session
-    ): Response {
+    public function initCallback(Request $request, SessionInterface $session): Response
+    {
         $numDice = $request->request->get('num_dices');
+
+        $hand = new DiceHand();
+        for ($i = 1; $i <= $numDice; $i++) {
+            $hand->add(new DiceGraphic());
+        }
+        $hand->roll();
+
+        $session->set("pig_dicehand", $hand);
 
         $session->set("pig_dices", $numDice);
         $session->set("pig_round", 0);
@@ -40,11 +46,15 @@ class DiceGameController extends AbstractController
         return $this->redirectToRoute('pig_play');
     }
     #[Route("/game/pig/play", name: "pig_play", methods: ['GET'])]
-    public function play(SessionInterface $session): Response {
+    public function play(SessionInterface $session): Response
+    {
+        $dicehand = $session->get("pig_dicehand");
+        
         $data = [
             "pigDices" => $session->get("pig_dices"),
             "pigRound" => $session->get("pig_round"),
             "pigTotal" => $session->get("pig_total"),
+            "diceValues" => $dicehand->getString() 
         ];
 
         return $this->render('pig/play.html.twig', $data);
