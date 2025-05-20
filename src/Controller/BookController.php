757c416130853510877c9e04d230680e81e9a1ca
hihassan1998 <hassanihussain1998@gmail.com>
2025-05-20 19:19:12 +0200

    Add Book class and its controller using Doctrine and add boo/create route

---
+++ src/Controller/BookController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use Doctrine\Persistence\ManagerRegistry;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route('/book/create', name: 'book_create')]
+    public function createProduct(
+        ManagerRegistry $doctrine
+    ): Response {
+        $entityManager = $doctrine->getManager();
+
+        $book = new Book();
+        $book->setTitle('Book Title ' . rand(1, 100));
+        $book->setIsbn('978-3-16-' . rand(1000000, 9999999));
+        $book->setAuthor('Author Name ' . rand(1, 10));
+        $book->setImage('book' . rand(1, 5) . '.jpg');
+
+        // tell Doctrine you want to (eventually) save the book
+        // (no queries yet)
+        $entityManager->persist($book);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new book with id ' . $book->getId());
+    }
+}
