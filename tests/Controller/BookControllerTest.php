e6d9eec210aadeaa835ec4c6b79f5f4d0d13cdf2
hihassan1998 <hassanihussain1998@gmail.com>
2025-05-25 19:27:27 +0200

    Update test

--- tests/Controller/BookControllerTest.php
+++ tests/Controller/BookControllerTest.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class BookControllerTest extends WebTestCase
 {
+    
     public function testIndex(): void
     {
         $client = static::createClient();
@@ -38,38 +39,6 @@ class BookControllerTest extends WebTestCase
 
     }
 
-    public function testShowBookById(): void
-    {
-        $client = static::createClient();
-
-        // We first need to have a book in DB to test
-        $container = static::getContainer();
-        /** @var EntityManagerInterface $em */
-        $em = $container->get(EntityManagerInterface::class);
-
-        $book = new Book();
-        $book->setTitle('Test Book');
-        $book->setIsbn('1234567890');
-        $book->setAuthor('Test Author');
-        $book->setImage('test.jpg');
-
-        $em->persist($book);
-        $em->flush();
-
-        $client->request('GET', '/book/show/' . $book->getId());
-
-        $this->assertResponseIsSuccessful();
-        $json = $client->getResponse()->getContent();
-        $this->assertIsString($json);
-        $responseData = json_decode($json, true);
-        $this->assertIsArray($responseData);
-
-
-        $this->assertEquals('Test Book', $responseData['title']);
-        $this->assertEquals('1234567890', $responseData['isbn']);
-        $this->assertEquals('Test Author', $responseData['author']);
-        $this->assertEquals('test.jpg', $responseData['image']);
-    }
 
     public function testNewBookFormDisplays(): void
     {
@@ -79,22 +48,7 @@ class BookControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists('form');
     }
-    public function testNewBookSubmit(): void
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/library/book/new');
 
-        $form = $crawler->selectButton('Save')->form();
-        $form['book_type_form[title]'] = 'New Book';
-        $form['book_type_form[isbn]'] = '111-222-333';
-        $form['book_type_form[author]'] = 'Author';
-        $form['book_type_form[image]'] = 'image.jpg';
-
-        $client->submit($form);
-        $client->followRedirect();
-
-        $this->assertRouteSame('book_view_all');
-    }
 
     public function testMetrics(): void
     {
