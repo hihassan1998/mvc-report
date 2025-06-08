43e4c2012d539f4f6190fb7a5c5480c4c1553f74
hihassan1998 <hassanihussain1998@gmail.com>
2025-06-08 12:11:00 +0200

    Add a POST route proj_goal_search  to display search results from the availbe Gobal, make changes in main/footer stylings,  add stylings to the POST form and display on porj/api route for trying

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -6,6 +6,8 @@ use App\Repository\GoalArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 /**
  * Controller for the 17 gobal goals project.
@@ -73,13 +75,49 @@ final class ProjectController extends AbstractController
     }
 
     /**
-     * Displays a simple start page for the /proj route.
+     * Displays a simple Json api listings page for the /proj/api route.
      *
      * @return Response
      */
     #[Route('/proj/api', name: 'app_proj_api')]
     public function projApi(): Response
     {
-        return $this->render('proj/proj-api.html.twig');
+        return $this->render('proj/proj_api.html.twig');
     }
+
+    #[Route('/proj/goals/search', name: 'proj_goal_search', methods: ['POST', 'GET'])]
+    public function searchGoals(
+        Request $request,
+        GoalArticleRepository $goalArticleRepository
+    ): Response {
+        $query = $request->request->get('query', '');
+
+        $goals = [];
+        if ($query) {
+            $qb = $goalArticleRepository->createQueryBuilder('g')
+                ->where('LOWER(g.name) LIKE :query')
+                ->orWhere('LOWER(g.defination) LIKE :query')
+                ->orWhere('LOWER(g.article) LIKE :query');
+
+            if (is_numeric($query)) {
+                $qb->orWhere('g.number = :number')
+                    ->setParameter('number', (int) $query);
+            }
+
+            $goals = $qb
+                ->setParameter('query', '%' . strtolower($query) . '%')
+                ->getQuery()
+                ->getResult();
+        }
+
+        if ($request->isXmlHttpRequest() || $request->getRequestFormat() === 'json' || $request->query->get('format') === 'json') {
+            return $this->json($goals, 200, [], ['json_encode_options' => JSON_PRETTY_PRINT]);
+        }
+
+        return $this->render('proj/goals_search_results.html.twig', [
+            'goals' => $goals,
+            'query' => $query,
+        ]);
+    }
+
 }
